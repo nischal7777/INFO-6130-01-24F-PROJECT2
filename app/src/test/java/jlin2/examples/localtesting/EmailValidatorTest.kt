69fc73e2063ef47c27fc7f8d4e9128dcf62c1594
Kanchan Chaudhary <kanchanchaudhary@Kanchans-MacBook-Air.local>
2024-11-23 20:41:26 -0500

    email test added

--- app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
+++ app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -22,6 +22,24 @@ class EmailValidatorTest {
     fun testInvalidEmailWithDoubleDots() {
         Assert.assertFalse(EmailValidator.isValidEmail("123@abc..com"))
     }
+    @Test
+    fun testInvalidEmailWithoutUsername() {
+        Assert.assertFalse(EmailValidator.isValidEmail("@abc.com"))
+    }
 
+    @Test
+    fun testInvalidEmailWithoutDomain() {
+        Assert.assertFalse(EmailValidator.isValidEmail("testing123"))
+    }
+
+    @Test
+    fun testEmptyEmail() {
+        Assert.assertFalse(EmailValidator.isValidEmail(""))
+    }
+
+    @Test
+    fun testNullEmail() {
+        Assert.assertFalse(EmailValidator.isValidEmail(null))
+    }
 
 }
